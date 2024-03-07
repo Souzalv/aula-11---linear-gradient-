8e4ff90837b6c3e711216c7ed188492d00253ef0
Souzalv <lucas.sa2005@gmail.com>
2024-03-07 13:59:47 -0300

    first commit

---
+++ solution/index.js
@@ -0,0 +1,15 @@
+// let nome = "Fernanda"
+// let idade = 25
+// let altura = 1.65
+// let temGato = false
+
+// // alert(`Olá eu sou a ${nome} tenho ${idade} anos e tenho ${altura}`)
+
+// let n1 = 10
+// let n2= 5
+
+// let resultado = n1*n2/2
+
+// alert(`A área do triângulo é de ${resultado}cm`)
+
+
